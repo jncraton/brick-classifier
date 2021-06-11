d9e41b11ea1a2331c568f9398769f9c743f31b8c
Jon Craton <jncraton@gmail.com>
2021-06-11 17:56:17 -0400

    Reformat with black

--- data/train.py
+++ data/train.py
@@ -21,36 +21,45 @@ import torch.onnx
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224, scale=(0.7,1.3), ratio=(1.0,1.0)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
+    "train": transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224, scale=(0.7, 1.3), ratio=(1.0, 1.0)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
 }
 
-data_dir = '.'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-                                          data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-                                             shuffle=True, num_workers=2)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+data_dir = "."
+image_datasets = {
+    x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+    for x in ["train", "val"]
+}
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=4, shuffle=True, num_workers=2
+    )
+    for x in ["train", "val"]
+}
+dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+class_names = image_datasets["train"].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 print(f"torch.device: {device}")
 
 # Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
+inputs, classes = next(iter(dataloaders["train"]))
 
 print(f"Classes: {json.dumps(class_names)}")
+
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
@@ -59,15 +68,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     best_acc = 0.0
 
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
+        for phase in ["train", "val"]:
+            if phase == "train":
                 model.train()  # Set model to training mode
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
@@ -82,43 +91,46 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                 # forward
                 # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
+                with torch.set_grad_enabled(phase == "train"):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
                     # backward + optimize only if in training phase
-                    if phase == 'train':
+                    if phase == "train":
                         loss.backward()
                         optimizer.step()
 
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train':
+            if phase == "train":
                 scheduler.step()
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == "val" and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
         print()
 
     time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print(
+        "Training complete in {:.0f}m {:.0f}s".format(
+            time_elapsed // 60, time_elapsed % 60
+        )
+    )
+    print("Best val Acc: {:4f}".format(best_acc))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
+
 
 feature_extractor_only = True
 
@@ -127,15 +139,15 @@ model = models.squeezenet1_1(pretrained=True)
 criterion = nn.CrossEntropyLoss()
 
 if feature_extractor_only:
-  for param in model.parameters():
-      param.requires_grad = False
+    for param in model.parameters():
+        param.requires_grad = False
 
-model.classifier = nn.Sequential (
+model.classifier = nn.Sequential(
     nn.Dropout(0.1),
-    nn.Conv2d(512, len(class_names), kernel_size= (1,1), stride= (1,1)),
-    nn.ReLU(), 
-    nn.AvgPool2d(kernel_size= 13, stride= 1, padding= 0)
-    )
+    nn.Conv2d(512, len(class_names), kernel_size=(1, 1), stride=(1, 1)),
+    nn.ReLU(),
+    nn.AvgPool2d(kernel_size=13, stride=1, padding=0),
+)
 model.num_classes = len(class_names)
 
 model = model.to(device)
@@ -143,31 +155,34 @@ model = model.to(device)
 # Parameters of newly constructed modules have requires_grad=True by default
 # Observe that only parameters of final layer are being optimized as
 # opposed to before.
-#optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
+# optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
 optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-model = train_model(model, criterion, optimizer, exp_lr_scheduler,
-                      num_epochs=25)
+model = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=25)
 
 batch_size = 1
 
-cpu_model = model.to('cpu')
+cpu_model = model.to("cpu")
 
 # Input to the model
 x = torch.randn(batch_size, 3, 224, 224, requires_grad=True)
 output = cpu_model(x)
 
 # Export the model
-torch.onnx.export(cpu_model,               # model being run
-                  x,                         # model input (or a tuple for multiple inputs)
-                  "part-classifier.onnx",    # where to save the model (can be a file or file-like object)
-                  export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=9,          # the ONNX version to export the model to
-                  do_constant_folding=True,  # whether to execute constant folding for optimization
-                  input_names = ['input'],   # the model's input names
-                  output_names = ['output'], # the model's output names
-                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-                                'output' : {0 : 'batch_size'}})
+torch.onnx.export(
+    cpu_model,  # model being run
+    x,  # model input (or a tuple for multiple inputs)
+    "part-classifier.onnx",  # where to save the model (can be a file or file-like object)
+    export_params=True,  # store the trained parameter weights inside the model file
+    opset_version=9,  # the ONNX version to export the model to
+    do_constant_folding=True,  # whether to execute constant folding for optimization
+    input_names=["input"],  # the model's input names
+    output_names=["output"],  # the model's output names
+    dynamic_axes={
+        "input": {0: "batch_size"},  # variable length axes
+        "output": {0: "batch_size"},
+    },
+)
